6940d0d8276cab6cddaf52a540f034b3396e1dc5
Batuhan Özbakir <bozbakir06@gmail.com>
2024-11-01 17:25:48 +0300

    Add project files.

---
+++ BaseCafe.DAL/Entities/BaseEntity.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaseCafe.DAL.Entities
+{
+    public abstract class BaseEntity
+    {
+        public int Id { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime ModifedDate { get; set; } = DateTime.MinValue;
+        public DateTime DeletedDate { get; set; } = DateTime.MinValue;
+        public DataStatus Status { get; set; }
+    }
+}
