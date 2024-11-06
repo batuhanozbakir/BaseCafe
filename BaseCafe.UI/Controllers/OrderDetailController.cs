d064c93ad262fa1f210799a8db0c1befaf30ed3b
Batuhan Özbakir <bozbakir06@gmail.com>
2024-11-06 15:15:11 +0300

    OrderDetailController oluşturuldu

--- BaseCafe.UI/Controllers/OrderDetailController.cs
+++ BaseCafe.UI/Controllers/OrderDetailController.cs
@@ -10,17 +10,54 @@ namespace BaseCafe.UI.Controllers
 		private readonly IGenericManager<ProductDTO, Product> _productManager;
 		private readonly IGenericManager<OrderDTO, Order> _orderManager;
 		private readonly IGenericManager<OrderDetailDTO, OrderDetail> _orderDetailManager;
+		private readonly IGenericManager<CategoryDTO, Category> _categoryManager;
 
-		public OrderDetailController(IGenericManager<ProductDTO, Product> productManager, IGenericManager<OrderDTO, Order> orderManager, IGenericManager<OrderDetailDTO, OrderDetail> orderDetailManager)
+		public OrderDetailController(IGenericManager<ProductDTO, Product> productManager, IGenericManager<OrderDTO, Order> orderManager, IGenericManager<OrderDetailDTO, OrderDetail> orderDetailManager, IGenericManager<CategoryDTO, Category> categoryManager)
 		{
 			_productManager = productManager;
 			_orderManager = orderManager;
 			_orderDetailManager = orderDetailManager;
+			_categoryManager = categoryManager;
 		}
 		public IActionResult Index()
 		{
+            var orders = _orderManager.GetAll();
 
-			return View();
-		}
-	}
+            var orderDetails = _orderDetailManager.GetAll();
+
+            var orderDtos = orders.Select(o => new
+
+            {
+
+                o.Id,
+
+                o.OrderDate,
+
+                o.TotalAmount,
+
+                o.Status,
+
+                OrderDetails = orderDetails.Where(od => od.OrderId == o.Id).Select(od => new
+
+                {
+
+                    od.ProductId,
+
+                    od.Quantity,
+
+                    od.UnitPrice,
+
+                    ProductName = _productManager.Find(od.ProductId)?.Name,
+
+                    CategoryName = _categoryManager.Find(_productManager.Find(od.ProductId)?.CategoryID ?? 0)?.Name
+
+                }).ToList()
+
+
+            }).ToList();
+
+            return View(orderDtos);
+
+        }
+    }
 }
